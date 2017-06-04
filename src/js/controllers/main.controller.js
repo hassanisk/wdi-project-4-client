7d0f90e03617af9f7d7335d1b601fa3dc4f0880c
hassanisk <hiskaf1@gmail.com>
2017-06-04 20:06:33 +0100

    ng repeat added

--- src/js/controllers/main.controller.js
+++ src/js/controllers/main.controller.js
@@ -3,8 +3,8 @@ angular
 .controller('MainCtrl', MainCtrl);
 
 
-MainCtrl.$inject = ['$rootScope','CurrentUserService','$state'];
-function MainCtrl($rootScope,CurrentUserService,$state) {
+MainCtrl.$inject = ['$rootScope','CurrentUserService','$state','$scope'];
+function MainCtrl($rootScope,CurrentUserService,$state, $scope) {
   const vm = this;
   $rootScope.$on('loggedIn', () => {
     vm.user = CurrentUserService.currentUser;
@@ -17,6 +17,65 @@ function MainCtrl($rootScope,CurrentUserService,$state) {
     vm.user = null;
     $state.go('login');
   });
+  //
+  // vm.exercises = [
+  //   {
+  //     name: 'biceps curls',
+  //     desc: 'this is a cool exercises',
+  //     rep: '10*10*10',
+  //     image: 'http://cdn-mf1.heartyhosting.com/sites/mensfitness.com/files/hammer_cheat_curl_main.jpg',
+  //     level: 'easy'
+  //   },
+  //   {
+  //     name: 'biceps preach',
+  //     desc: 'this is a cool exercises',
+  //     rep: '10*10*10*10',
+  //     image: 'http://cdn-mf0.heartyhosting.com/sites/mensfitness.com/files/band-curl-arms-exercise_landscape.jpg',
+  //     level: 'hard'
+  //   },
+  //   {
+  //     name: 'biceps cable',
+  //     desc: 'this is a cool exercises',
+  //     rep: '10*10*10*10',
+  //     image: 'http://build-muscle-101.com/wp-content/uploads/2014/12/high-pulley-cable-curl-ex.jpg',
+  //     level: 'medium'
+  //   }
+  // ];
+
+  $scope.myData = {};
+  $scope.myData.exercises = [
+    {
+      name: 'biceps curls',
+      desc: 'this is an easy exercises',
+      rep: '10*10*10',
+      image: 'http://cdn-mf1.heartyhosting.com/sites/mensfitness.com/files/hammer_cheat_curl_main.jpg',
+      level: 'easy'
+    },
+    {
+      name: 'biceps preach',
+      desc: 'this is a hard exercises',
+      rep: '10*10*10*10',
+      image: 'http://cdn-mf0.heartyhosting.com/sites/mensfitness.com/files/band-curl-arms-exercise_landscape.jpg',
+      level: 'hard'
+    },
+    {
+      name: 'biceps cable',
+      desc: 'this is a medium exercises',
+      rep: '10*10*10*10',
+      image: 'http://build-muscle-101.com/wp-content/uploads/2014/12/high-pulley-cable-curl-ex.jpg',
+      level: 'medium'
+    }
+  ];
+
+  $scope.myData.doClick = function(exercise) {
+    $scope.name = exercise.name;
+    $scope.desc = exercise.desc;
+    $scope.rep = exercise.rep;
+    $scope.image = exercise.image;
+    $scope.level = exercise.level;
+
+
+  };
 
 
 }
