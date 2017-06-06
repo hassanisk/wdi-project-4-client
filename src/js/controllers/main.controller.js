7b50ffece86598169278d62f77b57d3ab4159541
hassanisk <hiskaf1@gmail.com>
2017-06-06 10:31:18 +0100

    adding filter

--- src/js/controllers/main.controller.js
+++ src/js/controllers/main.controller.js
@@ -3,9 +3,10 @@ angular
 .controller('MainCtrl', MainCtrl);
 
 
-MainCtrl.$inject = ['$rootScope','CurrentUserService','$state','$scope', 'filterFilter'];
-function MainCtrl($rootScope,CurrentUserService,$state, $scope, filterFilter) {
+MainCtrl.$inject = ['$rootScope','CurrentUserService','$state','$scope', 'filterFilter', 'Exercise'];
+function MainCtrl($rootScope,CurrentUserService,$state, $scope, filterFilter, Exercise) {
   const vm = this;
+  vm.all = Exercise.query();
   $rootScope.$on('loggedIn', () => {
     vm.user = CurrentUserService.currentUser;
     // $state.go('tasksIndex'); //needs to be fixed
@@ -18,7 +19,13 @@ function MainCtrl($rootScope,CurrentUserService,$state, $scope, filterFilter) {
     $state.go('login');
   });
 
-
+  $scope.myData.doClick = function(exercise) {
+    $scope.name = exercise.name;
+    $scope.desc = exercise.desc;
+    $scope.rep = exercise.rep;
+    $scope.image = exercise.image;
+    $scope.level = exercise.level;
+  };
   $scope.getRandomIndex = function(){
     var index = Math.floor(Math.random() * $scope.myData.exercises.length);
     console.log(index);
@@ -27,21 +34,18 @@ function MainCtrl($rootScope,CurrentUserService,$state, $scope, filterFilter) {
     console.log($scope.item.name);
   };
 
-
-  filterExercises();
   function filterExercises() {
-    const params = { bodypart: vm.q };
-    if (vm.easy) params.size  = 'easy';
-    if (vm.medium) params.size = 'medium';
-    if (vm.hard) params.size  = 'hard';
-
-    vm.filtered = filterFilter(vm.myData, params);
+    const params = { name: vm.q };
+    if (vm.useLevel) params.level = vm.level;
+    if (vm.useBodypart) params.bodypart       = vm.bodypart;
+    vm.filtered = filterFilter(vm.all, params);
   }
   $scope.$watchGroup([
-    () => vm.easy,
-    () => vm.medium,
-    () => vm.hard,
-    () => vm.q
+    () => vm.q,
+    () => vm.level,
+    () => vm.useLevel,
+    () => vm.bodypart,
+    () => vm.useBodypart
   ], filterExercises);
 
   vm.filterExercises = filterExercises;
@@ -49,117 +53,6 @@ function MainCtrl($rootScope,CurrentUserService,$state, $scope, filterFilter) {
 
 
 
-  $scope.myData = {};
-  $scope.myData.exercises = [
-    { bodypart: 'biceps',
-    name: 'biceps curls',
-    desc: 'this is an easy exercises',
-    rep: '10*10*10',
-    image: 'http://cdn-mf1.heartyhosting.com/sites/mensfitness.com/files/hammer_cheat_curl_main.jpg',
-    level: 'easy'
-  },
-  {
-    bodypart: 'biceps',
-    name: 'biceps preach',
-    desc: 'this is a hard exercises',
-    rep: '10*10*10*10',
-    image: 'http://cdn-mf0.heartyhosting.com/sites/mensfitness.com/files/band-curl-arms-exercise_landscape.jpg',
-    level: 'hard'
-  },
-  {
-    bodypart: 'biceps',
-    name: 'biceps cable',
-    desc: 'this is a medium exercises',
-    rep: '10*10*10*10',
-    image: 'http://build-muscle-101.com/wp-content/uploads/2014/12/high-pulley-cable-curl-ex.jpg',
-    level: 'medium'
-  },
-  {
-    bodypart: 'legs',
-    name: 'squat',
-    desc: 'this is a hard exercises',
-    rep: '10*10*10*10',
-    image: 'http://cdn-mf0.heartyhosting.com/sites/mensfitness.com/files/band-curl-arms-exercise_landscape.jpg',
-    level: 'hard'
-  },
-  {
-    bodypart: 'legs',
-    name: 'deadlift',
-    desc: 'this is a hard exercises',
-    rep: '10*10*10*10',
-    image: 'http://cdn-mf0.heartyhosting.com/sites/mensfitness.com/files/band-curl-arms-exercise_landscape.jpg',
-    level: 'easy'
-  },
-  {
-    bodypart: 'chest',
-    name: 'bench press',
-    desc: 'this is a hard exercises',
-    rep: '10*10*10*10',
-    image: 'http://cdn-mf0.heartyhosting.com/sites/mensfitness.com/files/band-curl-arms-exercise_landscape.jpg',
-    level: 'medium'
-  },
-  {
-    bodypart: 'abs',
-    name: 'crunches',
-    desc: 'this is a easy exercises',
-    rep: '10*10*10*10',
-    image: 'http://cdn-mf0.heartyhosting.com/sites/mensfitness.com/files/band-curl-arms-exercise_landscape.jpg',
-    level: 'easy'
-  },
-  {
-    bodypart: 'abs',
-    name: 'crunches',
-    desc: 'this is a easy exercises',
-    rep: '10*10*10*10',
-    image: 'http://cdn-mf0.heartyhosting.com/sites/mensfitness.com/files/band-curl-arms-exercise_landscape.jpg',
-    level: 'easy'
-  },
-  {
-    bodypart: 'shoulder',
-    name: 'shoulder press',
-    desc: 'this is a easy exercises',
-    rep: '10*10*10*10',
-    image: 'http://cdn-mf0.heartyhosting.com/sites/mensfitness.com/files/band-curl-arms-exercise_landscape.jpg',
-    level: 'medium'
-  },
-  {
-    bodypart: 'triceps',
-    name: 'triceps extension',
-    desc: 'this is a easy exercises',
-    rep: '10*10*10*10',
-    image: 'http://cdn-mf0.heartyhosting.com/sites/mensfitness.com/files/band-curl-arms-exercise_landscape.jpg',
-    level: 'savage'
-  }
-];
-
-$scope.myData.doClick = function(exercise) {
-  $scope.name = exercise.name;
-  $scope.desc = exercise.desc;
-  $scope.rep = exercise.rep;
-  $scope.image = exercise.image;
-  $scope.level = exercise.level;
-};
-
-$scope.bodyparts = [
-  {
-    name: 'chest'
-  },
-  {
-    name: 'abs'
-  },
-  {
-    name: 'shoulders'
-  },
-  {
-    name: 'back'
-  },
-  {
-    name: 'biceps'
-  },
-  {
-    name: 'triceps'
-  }
-];
 
 
 }
--- src/js/factories/exersice.js
+++ src/js/factories/exersice.js
@@ -1,100 +1,83 @@
-// angular
-//   .module('gymApp')
-//   .factory('Exercise', Exercise);
-//
-// Exercise.$inject = [];
-// function Exercise() {
-//   const all = [
-//       { bodypart: 'biceps',
-//       name: 'biceps curls',
-//       desc: 'this is an easy exercises',
-//       rep: '10*10*10',
-//       image: 'http://cdn-mf1.heartyhosting.com/sites/mensfitness.com/files/hammer_cheat_curl_main.jpg',
-//       level: 1
-//     },
-//     {
-//       bodypart: 'biceps',
-//       name: 'biceps preach',
-//       desc: 'this is a hard exercises',
-//       rep: '10*10*10*10',
-//       image: 'http://cdn-mf0.heartyhosting.com/sites/mensfitness.com/files/band-curl-arms-exercise_landscape.jpg',
-//       level: 2
-//     },
-//     {
-//       bodypart: 'biceps',
-//       name: 'biceps cable',
-//       desc: 'this is a medium exercises',
-//       rep: '10*10*10*10',
-//       image: 'http://build-muscle-101.com/wp-content/uploads/2014/12/high-pulley-cable-curl-ex.jpg',
-//       level: 2
-//     },
-//     {
-//       bodypart: 'legs',
-//       name: 'squat',
-//       desc: 'this is a hard exercises',
-//       rep: '10*10*10*10',
-//       image: 'http://cdn-mf0.heartyhosting.com/sites/mensfitness.com/files/band-curl-arms-exercise_landscape.jpg',
-//       level: 3
-//     },
-//     {
-//       bodypart: 'legs',
-//       name: 'deadlift',
-//       desc: 'this is a hard exercises',
-//       rep: '10*10*10*10',
-//       image: 'http://cdn-mf0.heartyhosting.com/sites/mensfitness.com/files/band-curl-arms-exercise_landscape.jpg',
-//       level: 3
-//     },
-//     {
-//       bodypart: 'chest',
-//       name: 'bench press',
-//       desc: 'this is a hard exercises',
-//       rep: '10*10*10*10',
-//       image: 'http://cdn-mf0.heartyhosting.com/sites/mensfitness.com/files/band-curl-arms-exercise_landscape.jpg',
-//       level: 4
-//     },
-//     {
-//       bodypart: 'abs',
-//       name: 'crunches',
-//       desc: 'this is a easy exercises',
-//       rep: '10*10*10*10',
-//       image: 'http://cdn-mf0.heartyhosting.com/sites/mensfitness.com/files/band-curl-arms-exercise_landscape.jpg',
-//       level: 4
-//     },
-//     {
-//       bodypart: 'abs',
-//       name: 'crunches',
-//       desc: 'this is a easy exercises',
-//       rep: '10*10*10*10',
-//       image: 'http://cdn-mf0.heartyhosting.com/sites/mensfitness.com/files/band-curl-arms-exercise_landscape.jpg',
-//       level: 1
-//     },
-//     {
-//       bodypart: 'shoulder',
-//       name: 'shoulder press',
-//       desc: 'this is a easy exercises',
-//       rep: '10*10*10*10',
-//       image: 'http://cdn-mf0.heartyhosting.com/sites/mensfitness.com/files/band-curl-arms-exercise_landscape.jpg',
-//       level: 3
-//     },
-//     {
-//       bodypart: 'triceps',
-//       name: 'triceps extension',
-//       desc: 'this is a easy exercises',
-//       rep: '10*10*10*10',
-//       image: 'http://cdn-mf0.heartyhosting.com/sites/mensfitness.com/files/band-curl-arms-exercise_landscape.jpg',
-//       level: 2
-//     }
-//   ];
-//   return {
-//     query() {
-//       return all;
-//     },
-//     get(params) {
-//       return all.find((el) => {
-//         return Object.keys(params).every((key) => {
-//           return params[key] === el[key];
-//         });
-//       });
-//     }
-//   };
-// }
+angular
+.module('gymApp')
+.factory('Exercise', Exercise);
+
+Exercise.$inject = [];
+function Exercise() {
+  const all = [{
+    bodypart: 'biceps',
+    name: 'biceps curls',
+    desc: 'this is an easy exercises',
+    rep: '10*10*10',
+    image: 'http://cdn-mf1.heartyhosting.com/sites/mensfitness.com/files/hammer_cheat_curl_main.jpg',
+    level: 1
+  },{
+    bodypart: 'biceps',
+    name: 'biceps preach',
+    desc: 'this is a hard exercises',
+    rep: '10*10*10*10',
+    image: 'http://cdn-mf0.heartyhosting.com/sites/mensfitness.com/files/band-curl-arms-exercise_landscape.jpg',
+    level: 3
+  },{
+    bodypart: 'biceps',
+    name: 'biceps cable',
+    desc: 'this is a medium exercises',
+    rep: '10*10*10*10',
+    image: 'http://build-muscle-101.com/wp-content/uploads/2014/12/high-pulley-cable-curl-ex.jpg',
+    level: 2
+  },{
+    bodypart: 'legs',
+    name: 'squat',
+    desc: 'this is a hard exercises',
+    rep: '10*10*10*10',
+    image: 'http://cdn-mf0.heartyhosting.com/sites/mensfitness.com/files/band-curl-arms-exercise_landscape.jpg',
+    level: 3
+  },{
+    bodypart: 'legs',
+    name: 'deadlift',
+    desc: 'this is a hard exercises',
+    rep: '10*10*10*10',
+    image: 'http://cdn-mf0.heartyhosting.com/sites/mensfitness.com/files/band-curl-arms-exercise_landscape.jpg',
+    level: 1
+  },{
+    bodypart: 'chest',
+    name: 'bench press',
+    desc: 'this is a hard exercises',
+    rep: '10*10*10*10',
+    image: 'http://cdn-mf0.heartyhosting.com/sites/mensfitness.com/files/band-curl-arms-exercise_landscape.jpg',
+    level: 2
+  },{
+    bodypart: 'abs',
+    name: 'crunches',
+    desc: 'this is a easy exercises',
+    rep: '10*10*10*10',
+    image: 'http://cdn-mf0.heartyhosting.com/sites/mensfitness.com/files/band-curl-arms-exercise_landscape.jpg',
+    level: 1
+  },{
+    bodypart: 'abs',
+    name: 'crunches',
+    desc: 'this is a easy exercises',
+    rep: '10*10*10*10',
+    image: 'http://cdn-mf0.heartyhosting.com/sites/mensfitness.com/files/band-curl-arms-exercise_landscape.jpg',
+    level: 3
+  },{
+    bodypart: 'shoulders',
+    name: 'shoulder press',
+    desc: 'this is a easy exercises',
+    rep: '10*10*10*10',
+    image: 'http://cdn-mf0.heartyhosting.com/sites/mensfitness.com/files/band-curl-arms-exercise_landscape.jpg',
+    level: 'medium'
+  },{
+    bodypart: 'triceps',
+    name: 'triceps extension',
+    desc: 'this is a easy exercises',
+    rep: '10*10*10*10',
+    image: 'http://cdn-mf0.heartyhosting.com/sites/mensfitness.com/files/band-curl-arms-exercise_landscape.jpg',
+    level: 4
+  }];
+  return {
+    query() {
+      return all;
+    }
+  };
+}
