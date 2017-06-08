0cce79896aed7e6cad9c918bdbb26107f99d070f
hassanisk <hiskaf1@gmail.com>
2017-06-08 16:20:34 +0100

    adding parts

--- src/js/controllers/main.controller.js
+++ src/js/controllers/main.controller.js
@@ -64,4 +64,37 @@ function MainCtrl($rootScope,CurrentUserService,$state, $scope, filterFilter, Ex
       }
     }
   }
+
+  vm.biceps = function() {
+    console.log('clicked');
+    var bicep = document.getElementsByClassName('biLeft')[0];
+    if (bicep.style.opacity === '1') {
+      bicep.style.opacity = 0;
+      vm.bodypart = '';
+    } else {
+      console.log(bicep);
+      bicep.style.opacity = 1;
+      var bicepButton = document.getElementById('r1');
+      console.log(bicepButton);
+      bicepButton.checked = true;
+      vm.bodypart = bicepButton.value;
+      console.log(vm.bodypart);
+    }
+  };
+  vm.legsBack = function() {
+    console.log('clicked');
+    var leg = document.getElementsByClassName('legsBack')[0];
+    if (leg.style.opacity === '1') {
+      leg.style.opacity = 0;
+      vm.bodypart = '';
+    } else {
+      console.log(leg);
+      leg.style.opacity = 1;
+      var legButton = document.getElementById('r2');
+      console.log(legButton);
+      legButton.checked = true;
+      vm.bodypart = legButton.value;
+      console.log(vm.bodypart);
+    }
+  };
 }
