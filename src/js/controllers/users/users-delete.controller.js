f2a7583216f5f02814ec40a192020bf502c89cc5
hassanisk <hiskaf1@gmail.com>
2017-06-12 10:22:09 +0100

    user delete show done

--- src/js/controllers/users/users-delete.controller.js
+++ src/js/controllers/users/users-delete.controller.js
@@ -1,20 +1,20 @@
-angular
-  .module('gymApp')
-  .controller('UsersDeleteCtrl', UsersDeleteCtrl);
-
-
-UsersDeleteCtrl.$inject = ['User', '$state'];
-function UsersDeleteCtrl(User, $state) {
-  var vm = this;
-  vm.user = User;
-
-  function usersDelete() {
-    vm.user
-      .$remove()
-      .then(() => {
-        $state.go('home');
-      });
-  }
-
-  vm.delete = usersDelete;
-}
+// angular
+//   .module('gymApp')
+//   .controller('UsersDeleteCtrl', UsersDeleteCtrl);
+//
+//
+// UsersDeleteCtrl.$inject = ['User', '$state'];
+// function UsersDeleteCtrl(User, $state) {
+//   var vm = this;
+//   vm.user = User;
+//
+//   function usersDelete() {
+//     vm.user
+//       .$remove()
+//       .then(() => {
+//         $state.go('home');
+//       });
+//   }
+//
+//   vm.delete = usersDelete;
+// }
--- src/js/controllers/users/users-show.controller.js
+++ src/js/controllers/users/users-show.controller.js
@@ -2,8 +2,15 @@ angular
 .module('gymApp')
 .controller('UserShowCtrl', UserShowCtrl);
 
-UserShowCtrl.$inject = ['User', '$stateParams'];
-function UserShowCtrl(User, $stateParams){
+UserShowCtrl.$inject = ['User', '$stateParams','$state', 'CurrentUserService'];
+function UserShowCtrl(User, $stateParams, $state, CurrentUserService){
   const vm  = this;
   vm.user = User.get($stateParams);
+
+  vm.delete = () => {
+    User.delete($stateParams).$promise.then(() =>{
+      CurrentUserService.removeUser();
+      $state.go('home');
+    });
+  };
 }
